e3b60f106342ec14b97f1fa663a611796ef0d0b9
Ha Hai Nguyen <haiha.nguyen@etnetera.cz>
2018-06-22 23:25:06 +0200

    mapping the comp, set new imageUrl state, new functions, props for FR

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 
 // initialize with your api key. This will also work in your browser via http://browserify.org/
@@ -34,15 +35,16 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('click');
-    app.models.predict(Clarifai.GENERAL_MODEL, "https://samples.clarifai.com/metro-north.jpg").then(
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(Clarifai.COLOR_MODEL, this.state.input).then(
       function (response) {
         // do something with response
         console.log(response);
@@ -63,7 +65,7 @@ class App extends Component {
         <ImageLinkForm
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit} />
-        {/*<FaceRecognition />*/}
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
