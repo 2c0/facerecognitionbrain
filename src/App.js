de9d54dc743a2197d6979fcfafe65aeac4dc2ff9
Ha Hai Nguyen <haiha.nguyen@etnetera.cz>
2018-06-27 23:10:44 +0200

    adding calculateFaceLocation function

--- src/App.js
+++ src/App.js
@@ -35,9 +35,19 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+  }
+
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
   }
@@ -47,15 +57,8 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(
-        function (response) {
-          // do something with response
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        },
-        function (err) {
-          // there was an error
-        }
-      );
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => console.log(err));
   }
 
   render() {
